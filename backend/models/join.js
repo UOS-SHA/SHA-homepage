aa2e804169517fc563c7f5db6f750a068a15c85f
[GitHub <[GitHub>
2026-02-15 16:08:51 +0900

    팀 복수선택 수정

--- backend/models/join.js
+++ backend/models/join.js
@@ -30,7 +30,7 @@ module.exports = (sequelize, DataTypes)=> {
         },
 
         team: {
-            type: DataTypes.ENUM('A', 'B', 'C', 'later'),
+            type: DataTypes.JSON,
             allowNull: false
         },
 
